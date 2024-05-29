9c1641a89df04d5b8011de5d04616d501fd6ab10
JustGodWork <discord1elie@gmail.com>
2024-05-29 18:20:23 +0200

    Added task 4 (Grab places from frontend)

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,9 +1,50 @@
 $(document).ready(function () {
-  fetch("http://localhost:5001/api/v1/places_search/", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({}),
+  const nameAmenity = [];
+  $("input:checkbox").click(function () {
+      if ($(this).is(":checked")) {
+      nameAmenity.push($(this).attr("data-name"));
+      } else {
+      const nameIndex = nameAmenity.indexOf($(this).attr("data-name"));
+      nameAmenity.splice(nameIndex, 1);
+      }
+      $(".amenities h4").text(nameAmenity.join(", "));
+  });
+
+  fetch("http://localhost:5001/api/v1/status/").then((response) => {
+      const apiStatus = document.getElementById("api_status");
+
+      if (response.statusText === "OK") {
+      apiStatus.classList.add("available");
+      } else {
+      apiStatus.classList.remove("available");
+      }
+  }).catch((error) => {
+      console.log("An error occurred: " + error);
+  });
+
+  $.ajax({
+    type: 'POST',
+    url: 'http://localhost:5001/api/v1/places_search',
+    data: '{}',
+    dataType: 'json',
+    contentType: 'application/json',
+    success: function (data) {
+      $('SECTION.places').append(data.map(place => {
+        return `<article>
+                  <div class="title_box">
+                    <h2>${place.name}</h2>
+                    <div class="price_by_night">${place.price_by_night}</div>
+                  </div>
+                  <div class="information">
+                    <div class="max_guest">${place.max_guest} Guests</div>
+                    <div class="number_rooms">${place.number_rooms} Bedrooms</div>
+                    <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
+                  </div>
+                  <div class="description">
+                    ${place.description}
+                  </div>
+                </article>`
+      }));
+    }
   });
 });
