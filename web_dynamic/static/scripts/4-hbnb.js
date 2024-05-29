ca5609f6053b161641611cd9386d5e03ac8b598d
JustGodWork <discord1elie@gmail.com>
2024-05-29 18:20:47 +0200

    Added task 5 (Search by amenity(ies)

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,9 +1,64 @@
-$(document).ready(function () {
-  fetch("http://localhost:5001/api/v1/places_search/", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({}),
+function getPlaces(options={}) {
+  $.ajax({
+    type: 'POST',
+    url: 'http://localhost:5001/api/v1/places_search',
+    data: JSON.stringify(options),
+    dataType: 'json',
+    contentType: 'application/json',
+    success: function (data) {
+      $('SECTION.places').append(data.map(place => {
+        return `<article>
+                  <div class="title_box">
+                    <h2>${place.name}</h2>
+                    <div class="price_by_night">${place.price_by_night}</div>
+                  </div>
+                  <div class="information">
+                    <div class="max_guest">${place.max_guest} Guests</div>
+                    <div class="number_rooms">${place.number_rooms} Bedrooms</div>
+                    <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
+                  </div>
+                  <div class="description">
+                    ${place.description}
+                  </div>
+                </article>`
+      }));
+    }
   });
+};
+
+$(document).ready(function () {
+  const nameAmenity = [];
+  const amenityIds = [];
+
+  $("input:checkbox").click(function () {
+      if ($(this).is(":checked")) {
+        nameAmenity.push($(this).attr("data-name"));
+        amenityIds.push($(this).attr("data-id"));
+      } else {
+        const nameIndex = nameAmenity.indexOf($(this).attr("data-name"));
+        const idIndex = amenityIds.indexOf($(this).attr("data-id"));
+        nameAmenity.splice(nameIndex, 1);
+        amenityIds.splice(idIndex, 1);
+      }
+      $(".amenities h4").text(nameAmenity.join(", "));
+  });
+
+  $("#search").click(function() {
+    $('SECTION.places').empty();
+    getPlaces({ amenities: amenityIds });
+  });
+
+  fetch("http://localhost:5001/api/v1/status/").then((response) => {
+      const apiStatus = document.getElementById("api_status");
+
+      if (response.statusText === "OK") {
+      apiStatus.classList.add("available");
+      } else {
+      apiStatus.classList.remove("available");
+      }
+  }).catch((error) => {
+      console.log("An error occurred: " + error);
+  });
+
+  getPlaces();
 });
