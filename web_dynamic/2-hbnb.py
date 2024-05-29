095772f1146bbf5fc2be1bc8ddda7bcde70a8ec5
Cedric002 <cedgnleblanc@gmail.com>
2024-05-29 16:12:23 +0200

    create 2-hbnb.html; 2-hbnb.py  and 1-hbnb.js

--- web_dynamic/2-hbnb.py
+++ web_dynamic/2-hbnb.py
@@ -19,12 +19,12 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/1-hbnb', strict_slashes=False)
+@app.route('/2-hbnb', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
     states = sorted(states, key=lambda k: k.name)
-    cache_id = str(uuid.uuid4())
+    cache_id = uuid.uuid4()
     st_ct = []
 
     for state in states:
@@ -36,7 +36,7 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    return render_template('1-hbnb.html',
+    return render_template('2-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,
