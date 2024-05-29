e684f9295945ec8cbdf19fbb7d4f4159fb14ad7b
JustGodWork <discord1elie@gmail.com>
2024-05-29 17:40:59 +0200

    Fix task 3 js file fetch method

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  const nameAmenity = [];
+  $("input:checkbox").click(function () {
+    if ($(this).is(":checked")) {
+      nameAmenity.push($(this).attr("data-name"));
+    } else {
+      const nameIndex = nameAmenity.indexOf($(this).attr("data-name"));
+      nameAmenity.splice(nameIndex, 1);
+    }
+    $(".amenities h4").text(nameAmenity.join(", "));
+  });
+  fetch("http://localhost:5001/api/v1/status/").then((response) => {
+    const apiStatus = document.getElementById("api_status");
+
+    if (response.statusText === "OK") {
+      apiStatus.classList.add("available");
+    } else {
+      console.log("It's not OK, API say that.");
+      apiStatus.classList.remove("available");
+    }
+  }).catch((error) => {
+    console.log("An error occurred: " + error);
+  });
+});
